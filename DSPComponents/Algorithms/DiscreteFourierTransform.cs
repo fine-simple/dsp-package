05ed0a67443b1849b59073a86850247bd9d56bbf
Ahmed Tawfik <at200000000@gmail.com>
2022-11-02 00:21:38 +0200

    [Finished] DFT test complete

--- DSPComponents/Algorithms/DiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/DiscreteFourierTransform.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DSPAlgorithms.DataStructures;
+using System.Numerics;
 
 namespace DSPAlgorithms.Algorithms
 {
@@ -12,10 +13,19 @@ namespace DSPAlgorithms.Algorithms
         public Signal InputTimeDomainSignal { get; set; }
         public float InputSamplingFrequency { get; set; }
         public Signal OutputFreqDomainSignal { get; set; }
-
+        public List<Complex> freqDomain;
         public override void Run()
         {
-            throw new NotImplementedException();
+            OutputFreqDomainSignal = new Signal(false, new List<float>(), new List<float>(), new List<float>());
+            
+            freqDomain =  AlgorithmsUtilities.FrequencyToTimeDomain(InputTimeDomainSignal.Samples);
+            
+            for (int i = 0; i < freqDomain.Count; i++)
+            {
+                OutputFreqDomainSignal.Frequencies.Add(i);
+                OutputFreqDomainSignal.FrequenciesAmplitudes.Add(Convert.ToSingle(freqDomain[i].Magnitude));
+                OutputFreqDomainSignal.FrequenciesPhaseShifts.Add(Convert.ToSingle(freqDomain[i].Phase));
+            }
         }
     }
 }
