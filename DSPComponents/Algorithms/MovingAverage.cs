d66b2bae752a8a780deddcaceb453ecccbcb66da
Ahmed Tawfik <at200000000@gmail.com>
2022-11-08 11:18:01 +0200

    [Added] Finished moving average algorithm

--- DSPComponents/Algorithms/MovingAverage.cs
+++ DSPComponents/Algorithms/MovingAverage.cs
@@ -9,14 +9,23 @@ namespace DSPAlgorithms.Algorithms
 {
     public class MovingAverage : Algorithm
     {
-        // TODO: Task 3
+        // TODO: Task 3 [Completed]
         public Signal InputSignal { get; set; }
         public int InputWindowSize { get; set; }
         public Signal OutputAverageSignal { get; set; }
  
         public override void Run()
         {
-            throw new NotImplementedException();
+            int L = InputSignal.Samples.Count - InputWindowSize + 1;
+            var samples = new float[L];
+            for (int i = 0; i < L; i++)
+            {
+                float sum = 0;
+                for (int j = i; j < i + InputWindowSize; j++)
+                    sum += InputSignal.Samples[j];
+                samples[i] = sum / InputWindowSize;
+            }
+            OutputAverageSignal = new Signal(samples.ToList(), false);
         }
     }
 }
