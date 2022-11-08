3cde3747f850fdae297fe36493353289e7675c44
Ahmed Tawfik <at200000000@gmail.com>
2022-11-08 11:18:26 +0200

    [Added] turn sample and sample indices to dictionary

--- DSPComponents/Algorithms/Utilities.cs
+++ DSPComponents/Algorithms/Utilities.cs
@@ -10,5 +10,16 @@ namespace DSPAlgorithms.Algorithms
     public class AlgorithmsUtilities
     {
         // Implement here any functionalities you need to share between Algorithm classes
+        public static Dictionary<int, float> getSampleDict(List<int> samplesIndices, List<float> samples)
+        {
+            var result = new Dictionary<int, float>();
+            for (int i = 0; i < samples.Count; i++)
+            {
+                int key = samplesIndices[i];
+                float value = samples[i];
+                result.Add(key, value);
+            }
+            return result;
+        }
     }
 }
