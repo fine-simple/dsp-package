26f360a8dd188b693eadb7b365ce18c05c5b1bed
Ahmed Tawfik <at200000000@gmail.com>
2022-11-10 06:12:12 +0200

    [Added] Task 3 New Files

---
+++ DSPComponents/Algorithms/AccumulationSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSPAlgorithms.DataStructures;
+
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class AccumulationSum : Algorithm
+    {
+        // TODO: Task 3
+        public Signal InputSignal { get; set; }
+        public Signal OutputSignal { get; set; }
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DSPComponents/Algorithms/Derivatives.cs
@@ -0,0 +1,24 @@
+﻿using DSPAlgorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class Derivatives: Algorithm
+    {
+        // TODO: Task 3
+        public Signal InputSignal { get; set; }
+        public Signal FirstDerivative { get; set; }
+        public Signal SecondDerivative { get; set; }
+
+        public override void Run()
+        {
+
+           throw new NotImplementedException();
+        }
+    }
+}
