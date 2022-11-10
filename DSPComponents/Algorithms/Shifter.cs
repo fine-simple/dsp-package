7b5c1370b56c16bdf17971d1632c534b2b7d6af2
Ahmed Tawfik <at200000000@gmail.com>
2022-11-10 06:11:42 +0200

    [Finished] Shifter Algorithm

--- DSPComponents/Algorithms/Shifter.cs
+++ DSPComponents/Algorithms/Shifter.cs
@@ -10,19 +10,20 @@ namespace DSPAlgorithms.Algorithms
     public class Shifter : Algorithm
     {
         // TODO: Task 3
-        // FIXME: When Folded the sign of shift value is reversed
         public Signal InputSignal { get; set; }
         public int ShiftingValue { get; set; }
         public Signal OutputShiftedSignal { get; set; }
 
         public override void Run()
         {
-            OutputShiftedSignal = new Signal(InputSignal.Samples.ToList(), false);
+            OutputShiftedSignal = new Signal(InputSignal.Samples.ToList(), InputSignal.SamplesIndices.ToList(), InputSignal.Periodic);
             
             var samplesN = OutputShiftedSignal.SamplesIndices;
 
+            int sign = InputSignal.Periodic ? 1 : -1;
+        
             for (int i = 0; i < samplesN.Count; i++)
-                samplesN[i] = InputSignal.SamplesIndices[i] - ShiftingValue;
+                samplesN[i] += sign * ShiftingValue;
         }
     }
 }
