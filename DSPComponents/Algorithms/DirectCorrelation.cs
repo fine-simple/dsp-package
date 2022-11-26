893a88e12f6c391167972250b4756afbf771537e
Ahmed Tawfik <at200000000@gmail.com>
2022-11-26 20:16:55 +0200

    [Finished] Task 5 Direct Correlation

--- DSPComponents/Algorithms/DirectCorrelation.cs
+++ DSPComponents/Algorithms/DirectCorrelation.cs
@@ -16,7 +16,66 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            if(InputSignal2 == null)
+                InputSignal2 = new Signal(InputSignal1.Samples.ToList(), InputSignal1.SamplesIndices.ToList(), InputSignal1.Periodic);
+            
+            OutputNonNormalizedCorrelation = new List<float>();
+            OutputNormalizedCorrelation = new List<float>();
+
+            float dominator = getNormalizingDominator(InputSignal1.Samples, InputSignal2.Samples);
+            
+            for (int i = 0; i < InputSignal1.Samples.Count; i++)
+            {
+                float r = getR(InputSignal1.Samples, InputSignal2.Samples);
+                OutputNonNormalizedCorrelation.Add(r);
+                OutputNormalizedCorrelation.Add(r / dominator);
+                shiftRight(InputSignal2);
+            }
+        }
+
+        private void shiftRight(Signal signal)
+		{
+            float[] newSamples = new float[signal.Samples.Count];
+
+			if(signal.Periodic)
+                newSamples[newSamples.Length - 1] = signal.Samples[0];
+            else 
+                newSamples[newSamples.Length - 1] = 0;
+
+            for (int i = 0; i < newSamples.Length - 1; i++)
+                newSamples[i] = signal.Samples[i+1];
+
+            signal.Samples = newSamples.ToList();
+		}
+		private float getNormalizingDominator(List<float> signal1, List<float> signal2)
+        {
+            float sum1 = getSquaredSum(signal1);
+            float sum2 = getSquaredSum(signal2);
+            float result = (float)Math.Sqrt(sum1 * sum2) / signal1.Count;
+            return result;
+        }
+
+        private float getSquaredSum(List<float> signal1)
+        {
+            float sum = 0;
+            for (int i = 0; i < signal1.Count; i++)
+            {
+                sum += signal1[i] * signal1[i];
+            }
+            return sum;
+        }
+
+        private float getR(List<float> signal1, List<float> signal2)
+        {
+            float sum = 0;
+            for (int i = 0; i < signal1.Count; i++)
+            {
+                sum += signal1[i] * signal2[i];
+            }
+            
+            float corr = sum / signal1.Count;
+            
+            return corr;
         }
     }
 }
