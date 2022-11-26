0f4d8787869142416733c7c780deea5f3fa0e80b
adhamali450 <adhamali_4500@outlook.com>
2022-11-26 20:46:35 +0200

    Time Delay ✅

--- DSPComponents/Algorithms/TimeDelay.cs
+++ DSPComponents/Algorithms/TimeDelay.cs
@@ -7,7 +7,7 @@ using DSPAlgorithms.DataStructures;
 
 namespace DSPAlgorithms.Algorithms
 {
-    public class TimeDelay:Algorithm
+    public class TimeDelay : Algorithm
     {
         public Signal InputSignal1 { get; set; }
         public Signal InputSignal2 { get; set; }
@@ -16,7 +16,25 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            DirectCorrelation corr = new DirectCorrelation();
+            corr.InputSignal1 = InputSignal1;
+            corr.InputSignal2 = InputSignal2;
+            corr.Run();
+
+            int maxIndex = 0;
+            float max = 0;
+            for (int i = 0; i < corr.OutputNormalizedCorrelation.Count; i++)
+            {
+                float curr = Math.Abs(corr.OutputNormalizedCorrelation[i]);
+                if (curr > max)
+                {
+                    max = curr;
+                    maxIndex = i;
+                }
+            }
+
+            OutputTimeDelay = maxIndex * InputSamplingPeriod;
+
         }
     }
 }
