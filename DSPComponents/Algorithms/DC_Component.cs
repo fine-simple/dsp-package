36d82e4d25b000689a3b9b7ddcb4f546d47be9c9
Ahmed Tawfik <at200000000@gmail.com>
2022-12-20 14:49:48 +0200

    [Added] Signal Indices to Output Signal

--- DSPComponents/Algorithms/DC_Component.cs
+++ DSPComponents/Algorithms/DC_Component.cs
@@ -15,15 +15,20 @@ namespace DSPAlgorithms.Algorithms
         public override void Run()
         {
             // Calculating avg.
-            float samplesSum = 0.0F, avg = 0.0F;
+            float samplesSum = 0.0F,
+                avg = 0.0F;
             foreach (float sample in InputSignal.Samples)
                 samplesSum += sample;
             avg = samplesSum / InputSignal.Samples.Count;
 
             // Removing avg. from samples
             OutputSignal = new Signal(new List<float>(), false);
+            OutputSignal.SamplesIndices = InputSignal.SamplesIndices.ToList();
+            
             foreach (float sample in InputSignal.Samples)
+            {
                 OutputSignal.Samples.Add(sample - avg);
+            }
         }
     }
 }
