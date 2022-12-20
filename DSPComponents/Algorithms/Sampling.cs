c796795d56306a5145bd2bcadd6118d3d25df11d
Ahmed Tawfik <at200000000@gmail.com>
2022-12-20 03:57:56 +0200

    [Finished] Sampling Task

--- DSPComponents/Algorithms/Sampling.cs
+++ DSPComponents/Algorithms/Sampling.cs
@@ -14,14 +14,67 @@ namespace DSPAlgorithms.Algorithms
         public Signal InputSignal { get; set; }
         public Signal OutputSignal { get; set; }
 
-
-
-
         public override void Run()
-        {
-            // throw new NotImplementedException();
+		{
+			var lowFilter = initFilter();
+            if(M == 0 && L != 0)
+            {
+                Signal output = sampleUp(InputSignal);
+                lowFilter.InputTimeDomainSignal = output;
+                lowFilter.Run();
+                OutputSignal = lowFilter.OutputYn;
+            }
+            else if(M != 0 && L == 0)
+            {
+                lowFilter.InputTimeDomainSignal = InputSignal;
+                lowFilter.Run();
+                OutputSignal = sampleDown(lowFilter.OutputYn);
+            }
+            else if(M != 0 && L != 0)
+            {
+                Signal upSamples = sampleUp(InputSignal);
+                lowFilter.InputTimeDomainSignal = upSamples;
+                lowFilter.Run();
+                OutputSignal = sampleDown(lowFilter.OutputYn);
+            }
+		}
+        private Signal sampleUp(Signal input) {
+            int N = L * input.Samples.Count + 1;
+            List<float> samples = new List<float>(N);
+            for (int i = 0; i < input.Samples.Count; i++)
+            {
+                samples.Add(input.Samples[i]);
+                for (int j = 0; j < L - 1; j++)
+                {
+                    samples.Add(0);
+                }
+            }
 
+            return new Signal(samples, input.Periodic);
         }
-    }
+
+        private Signal sampleDown(Signal input) {
+            int N = input.Samples.Count / M;
+            List<float> samples = new List<float>(N);
+
+            for (int i = 0; i < input.Samples.Count; i+= M)
+            {
+                samples.Add(input.Samples[i]);
+            }
+
+            return new Signal(samples, input.Periodic);
+        }
+
+		private static FIR initFilter()
+		{
+			var fir = new FIR();
+			fir.InputFilterType = FILTER_TYPES.LOW;
+			fir.InputFS = 8000;
+			fir.InputStopBandAttenuation = 50;
+			fir.InputCutOffFrequency = 1500;
+            fir.InputTransitionBand = 500;
+			return fir;
+		}
+	}
 
 }
